5faa3230105e9aebf04aae660a267f02f1115263
soneuik <soneuik12@gmail.com>
2020-02-21 17:22:33 -0800

    section1 리액트소개 끝

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -27,10 +27,15 @@ class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value}) } />
+                {/* onChange 통해서 state의 term이 업데이트 되고, 그럼 value에서 state의 term을 가져와서 업데이트한다
+                1.onChange
+                2.setState()
+                3.value = this.state.term */}
+                <input 
+                value={this.state.term}
+                onChange={event => this.setState({ term: event.target.value}) } />
                 {/* Value of the input: {this.state.term} */}
-            </div>
-
+            </div> 
         );
         
     } 
